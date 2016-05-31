0da67420e2f924b5445370ae9bb91e1a41a8a588
Patrick Callahan <pxcallahan@gmail.com>
2016-05-30 21:57:39 -0700

    enable gpg-agent

--- keychain.fish
+++ keychain.fish
@@ -6,8 +6,8 @@ function init --on-event init_keychain
   else
     if status --is-interactive
       # handle keychain versions that do not support --confhost
-      if not keychain --eval --quiet --confhost --timeout 60 2>/dev/null | source
-        keychain --eval --quiet --timeout 60 | source
+      if not keychain --eval --quiet --confhost --timeout 60 --agents ssh,gpg  2>/dev/null | source
+        keychain --eval --quiet --timeout 60 --agents ssh,gpg | source
       end
     end
   end
