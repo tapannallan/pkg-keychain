3e695a6a9db0aa2e2ea515dcbe01a940266f9478
Patrick Callahan <pxcallahan@gmail.com>
2016-05-30 22:01:10 -0700

    status → builtin status (because of upstart aliases)

--- keychain.fish
+++ keychain.fish
@@ -4,7 +4,7 @@ function init --on-event init_keychain
   if not type -q keychain
     echo "🔑  Please install 'keychain' first!"
   else
-    if status --is-interactive
+    if builtin status --is-interactive
       # handle keychain versions that do not support --confhost
       if not keychain --eval --quiet --confhost --timeout 60 --agents ssh,gpg  2>/dev/null | source
         keychain --eval --quiet --timeout 60 --agents ssh,gpg | source
